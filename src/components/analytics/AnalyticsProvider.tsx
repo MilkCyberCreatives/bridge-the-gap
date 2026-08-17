54bcca9d872d574a6d61e3caf6212a409c134f6e
Milk Cyber Creatives <milkcybercreatives@gmail.com>
2026-08-17 23:09:53 +0200

    Load analytics only after consent

--- src/components/analytics/AnalyticsProvider.tsx
+++ src/components/analytics/AnalyticsProvider.tsx
@@ -11,6 +11,7 @@ import { trackEvent, trackPageView } from "@/lib/tracking";
 export default function AnalyticsProvider() {
   const [allowAnalytics, setAllowAnalytics] = useState(false);
   const gaId = process.env.NEXT_PUBLIC_GA_ID || "";
+  const gtmId = process.env.NEXT_PUBLIC_GTM_ID || "GTM-W92N8LBX";
   const metaPixelId = process.env.NEXT_PUBLIC_META_PIXEL_ID || "";
   const pathname = usePathname();
   const searchParams = useSearchParams();
@@ -71,6 +72,16 @@ export default function AnalyticsProvider() {
     <>
       <Analytics />
       <SpeedInsights />
+
+      {gtmId ? (
+        <Script id="gtm-base" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`}
+        </Script>
+      ) : null}
 
       {gaId ? (
         <>
